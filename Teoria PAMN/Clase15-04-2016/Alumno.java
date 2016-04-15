22a543990f2fc7a3eb576fdad13bc28a1b76d62e
Germán Santos Jaimes <germans@unam.mx>
2016-04-15 13:08:12 -0500

    Usando variables static

--- Teoria PAMN/Clase15-04-2016/Alumno.java
+++ Teoria PAMN/Clase15-04-2016/Alumno.java
@@ -2,14 +2,18 @@ public class Alumno{
 	
 	String nombre;
 	int edad;
+	int matricula = 0;
+	static int contador = 0;
 
 	public Alumno(String nombre, int edad){
 		this.nombre = nombre;
 		this.edad = edad;
+		contador = contador + 1;
+		matricula = contador;
 	}
 
 	public String toString(){
-		return "Yo soy " + nombre + " tengo " + edad + " años";
+		return "Yo soy " + nombre + " tengo " + edad + " años y mi matricula es " + matricula;
 	}
 
 	void leer(){
