79ae66cae26c8fbbcb19e6fad55b9a1fd5202a9d
Germán Santos Jaimes <germans@unam.mx>
2016-04-25 16:38:00 -0500

    Acomodo de tablero

--- Fundamentos-Programacion/Laboratorio/Clase-25042016/naval.c
+++ Fundamentos-Programacion/Laboratorio/Clase-25042016/naval.c
@@ -60,7 +60,7 @@ void muestra_tablero(){
 		}
 		printf("\n");
 	}
-	printf("------------------");
+	printf("---------------------\n");
 	for(i=0;i<5;i++){
 		for(j=0;j<5;j++){
 			printf(" %c |", jugador[i][j]);
