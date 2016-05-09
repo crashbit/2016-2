d69b9dba412f3b6396a4ce46053229752830ff7b
Administrator <crashbito@gmail.com>
2016-05-09 11:24:24 -0500

    Formato de Registro

--- Teoria-PAMN/flask/Formularios_Registro/app.py
+++ Teoria-PAMN/flask/Formularios_Registro/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template
 #from app import forms.NameForm
 from forms import NameForm
+from forms import RegisterForm
 
 app = Flask(__name__)
 
@@ -16,6 +17,15 @@ def index():
 def formas():
 	form = NameForm()
 	return render_template("forma.html", form=form)
+
+@app.route('/registro', methods=['get', 'post'])
+def registro():
+	form = RegisterForm()
+	if form.validate_on_submit():
+		nombre = form.nombre.data
+		print nombre
+		form.nombre.data = ''
+	return render_template("registro2.html", form=form)
 
 if __name__ == "__main__":
 	app.run(debug=True)
--- Teoria-PAMN/flask/Formularios_Registro/forms.py
+++ Teoria-PAMN/flask/Formularios_Registro/forms.py
@@ -1,7 +1,14 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, SubmitField 
+from wtforms import StringField, PasswordField, SubmitField 
 from wtforms.validators import Required
+from wtforms.validators import Email
 
 class NameForm(Form):
 	name = StringField('Como te llamas?', validators=[Required()]) 
 	submit = SubmitField('Enviar')
+
+class RegisterForm(Form):
+	nombre = StringField('Nombre', validators=[Required()])
+	apellidos = StringField('Apellidos', validators=[Required()])
+	correo = StringField('Correo electronico', validators=[Required("Este campo se requiere"), Email()])
+	password = PasswordField('Contrasenna',validators=[Required("Este campo se requiere")])
